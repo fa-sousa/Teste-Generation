9c89f6132579e04a548770b30ffc6b8f98fa1548
fa-sousa <fsousa292@gmail.com>
2020-07-14 03:21:28 -0300

    Configura formulário e organiza conteudo das sections

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function Enviar() {
  
-  var nome = document.getElementById("nomeid");
+  var nome = document.getElementById("input-name");
 
   if (nome.value != "") {
       alert('Muito obrigades pelo seu contato ' + nome.value + ' !');
